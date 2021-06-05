f7ba309c4a823a2f08cdd38fad3db0750979caff
Aditya Khoirul Anam <dityaaa@outlook.com>
2021-06-06 06:16:03 +0700

    Remove unnecessary escape

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const matchers = {
   "phpunit-failure": {
-      "regexp": "##teamcity\\[testFailed.+message='(.+)'.+details='(?:\\s|\\|n\\s)+(?:.+\\|n[^'])?{{GITHUB_WORKSPACE}}\/([^:]+):(\\d+)[^']+'",
+      "regexp": "##teamcity\\[testFailed.+message='(.+)'.+details='(?:\\s|\\|n\\s)+(?:.+\\|n[^'])?{{GITHUB_WORKSPACE}}/([^:]+):(\\d+)[^']+'",
       "defaultSeverity": "error",
       "message": 1,
       "file": 2,
@@ -17,7 +17,7 @@ for (let matcher in matchers) {
         "owner": matcher,
         "severity": details.defaultSeverity,
         "pattern": [{
-          "regexp": details.regexp.replace("{{GITHUB_WORKSPACE}}", process.env.GITHUB_WORKSPACE.replace(/\//g, '\\/') || ''),
+          "regexp": details.regexp.replace("{{GITHUB_WORKSPACE}}", process.env.GITHUB_WORKSPACE || ''),
           "message": details.message,
           "file": details.file,
           "line": details.line
